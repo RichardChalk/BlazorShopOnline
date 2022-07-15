c0e269f0c8eeb2c2ff21ceec786e67369eba03c2
Richard Chalk <barmybrit@gmail.com>
2022-07-15 19:17:54 +0200

    Working on connectionsstring and DbContext

---
+++ ShopOnline.Api/Data/BlazorShopOnlineDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopOnline.Api.Data
+{
+    public class BlazorShopOnlineDbContext : DbContext
+    {
+        public BlazorShopOnlineDbContext(DbContextOptions<BlazorShopOnlineDbContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
--- ShopOnline.Api/Entities/User.cs
+++ ShopOnline.Api/Entities/User.cs
@@ -2,6 +2,8 @@
 {
     public class User
     {
+        //This class replaces Identity User 
+        // I wanted the app to be simpla as I'm trying to learn Blazor
         public int Id { get; set; }
         public string UserName { get; set; }
     }
